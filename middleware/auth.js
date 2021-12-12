9994e44941abd25484dde5ecebcdaec6bc3a3cf3
unknown <andyfow01@gmail.com>
2021-12-12 19:53:12 -0300

    .

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 
 function auth(req, res, next){
     try {
-        const token = req.header('Token');
+        //const token = req.header('Token');
         const token = req.header('Authorization').replace('Bearer ', '');
-        const token = req.header('Authorization');
+        //const token = req.header('Authorization');
         console.log(token);
         jwt.verify(token, process.env.SECRET);
         next();
